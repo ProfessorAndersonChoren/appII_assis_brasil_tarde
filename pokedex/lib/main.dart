b5065629367c52ebb495a41791fdefa0b5c2f93e
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-02-28 19:23:35 -0300

    Pokedex - Parte 1

---
+++ pokedex/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/screens/home/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Pokedex",
+      home: Home(),
+      theme: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFFE3350D),
+        ),
+        // useMaterial3: true,
+      ),
+      
+    ),
+  );
+}
