69142921400c8bacf5c1c7a1ced0341cf5b84985
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-03-07 15:52:44 -0300

    Pokedex - final

--- pokedex/lib/screens/home/home.dart
+++ pokedex/lib/screens/home/home.dart
@@ -10,78 +10,135 @@ class Home extends StatelessWidget {
         title: const Text("Charmander #004"),
         leading: Image.asset("assets/logo.png"),
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Image.asset("assets/charmander.png"),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Text(
-                  "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta da cauda."),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    // 1° Linha
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        // 1° Linha - 1° Coluna
-                        Column(
-                          children: [
-                            titleText(
-                              text: "Altura",
-                              context: context,
-                            ),
-                            subtitleText(
-                              text: "0.6m",
-                              context: context,
-                            )
-                          ],
-                        ),
-                        // 1° Linha - 2° Coluna
-                        Column(
-                          children: [
-                            titleText(
-                              text: "Peso",
-                              context: context,
-                            ),
-                            subtitleText(
-                              text: "8.5kg",
-                              context: context,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    // 2° Linha
-                    Row(
-                      children: [
-                        // 2° Linha - 1° Coluna
-                        Column(
-                          children: [
-                            titleText(
-                              text: "Tipo",
-                              context: context,
-                            ),
-                            customChip(
-                              label: "Fogo",
-                              backgroundColor: const Color(0xFFF17F2E),
-                              context: context,
-                            ),
-                          ],
-                        ),
-                        // 2° Linha - 2° Coluna
-                      ],
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 16,
+          ),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/charmander.png",
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                    "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta da cauda."),
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      // 1° Linha
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // 1° Linha - 1° Coluna
+                          Column(
+                            children: [
+                              titleText(
+                                text: "Altura",
+                                context: context,
+                              ),
+                              subtitleText(
+                                text: "0.6m",
+                                context: context,
+                              )
+                            ],
+                          ),
+                          // 1° Linha - 2° Coluna
+                          Column(
+                            children: [
+                              titleText(
+                                text: "Peso",
+                                context: context,
+                              ),
+                              subtitleText(
+                                text: "8.5kg",
+                                context: context,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      // 2° Linha
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // 2° Linha - 1° Coluna
+                          Column(
+                            children: [
+                              titleText(
+                                text: "Tipo",
+                                context: context,
+                              ),
+                              customChip(
+                                label: "Fogo",
+                                backgroundColor: const Color(0xFFF17F2E),
+                                context: context,
+                              ),
+                            ],
+                          ),
+                          // 2° Linha - 2° Coluna
+                          Column(
+                            children: [
+                              titleText(
+                                text: "Habilidade",
+                                context: context,
+                              ),
+                              subtitleText(
+                                text: "Chama",
+                                context: context,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 24,
+                    bottom: 8,
+                  ),
+                  child: Text(
+                    "Fraquezas",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  customChip(
+                    label: "Água",
+                    backgroundColor: const Color(0xFF688FF3),
+                    context: context,
+                  ),
+                  customChip(
+                    label: "Terra",
+                    backgroundColor: const Color(0xFFF6DE3E),
+                    context: context,
+                  ),
+                  customChip(
+                    label: "Rocha",
+                    backgroundColor: const Color(0xFFA48C22),
+                    context: context,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
